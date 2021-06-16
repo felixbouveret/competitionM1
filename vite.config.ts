94c7ab58399a115d94ebcc8e0d44dab0b74818ae
Félix Bouveret <bouveret.felix@gmail.com>
2021-06-16 14:53:45 +0200

    fix(vite.config): fix unknown imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    dynamicImportVars({
-      include: ['**/*.vue', '**/*.js'],
-      exclude: ['node_modules/**'],
-    }),
-
   ],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src") // map '@' to './src' 
+      "@": resolve(__dirname, "./src") // map '@' to './src' 
     },
   },
 })
