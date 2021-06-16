27de6712e8164dca2eb2420e257cde8a8da47880
Félix Bouveret <bouveret.felix@gmail.com>
2021-06-16 09:26:10 +0200

    feat(core): setup main app architecture

--- src/modules/Homepage/Homepage.vue
+++ src/modules/Homepage/Homepage.vue
@@ -1,16 +1,25 @@
 <template>
-  <Homepage />
+  <img alt="Vue logo" :src="Logo" />
+  <Hero msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Homepage from './modules/Homepage'
+import { Hero } from './components'
+import Logo from '@/assets/logo.png'
 
 export default defineComponent({
   name: 'App',
+  
   components: {
-    Homepage
-}
+    Hero,
+  },
+
+  setup() {
+    return {
+      Logo
+    }
+  }
 })
 </script>
 
