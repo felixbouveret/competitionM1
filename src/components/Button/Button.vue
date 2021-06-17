75e8e7e5adf090f16bc69510a7f5ec35a7b1a920
Félix Bouveret <bouveret.felix@gmail.com>
2021-06-17 16:06:23 +0200

    feat(Button): add global button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,54 @@
+<template>
+  <a 
+    v-if="href"
+    :href="href"
+    :class="[$style.button, {[$style.isLight]: isLight}]"
+  >
+    <slot />
+  </a>
+  <button
+    v-else
+    :class="[$style.button, {[$style.isLight]: isLight}]"
+    @click="()=>$emit('on-click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Button',
+
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    isLight: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  emits: ['on-click'],
+})
+</script>
+
+<style lang="scss" module>
+  .button {
+    padding: 20px;
+    background-color: $black;
+    border: 0;
+    color: $white;
+    cursor: pointer;
+
+    @include typo-headline;
+  }
+
+  .isLight {
+    background-color: $white;
+    color: $black;
+  }
+</style>
