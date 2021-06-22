bb6e1a9d35afa62298b6d8c2d2f8f9e16c9692af
Félix Bouveret <bouveret.felix@gmail.com>
2021-06-22 20:16:31 +0200

    fix(UneHistoire): refacto responsive

--- src/modules/HomePage/components/UneHistoire/UneHistoire.vue
+++ src/modules/HomePage/components/UneHistoire/UneHistoire.vue
@@ -1,22 +1,23 @@
 <template>
-  <section :class="$style.histoireback">
-    <Wrapper>
-      <div :class="$style.histoirecentre">
-        <div :class="$style.histoiregauche">
-          <img :src="Persil">
-        </div>
-        <div :class="$style.histoiredroite">
-          <div>
-            <h3 :class="$style.h3left">
-              UNE HISTOIRE <br> D’humains
-            </h3>
-            <p :class="$style.texthistoire">
+  <section :class="$style.container">
+    <Wrapper :class="$style.inner">
+      <img
+        :class="$style.view"
+        :src="Persil"
+      >
+      <div :class="$style.content">
+        <div>
+          <h3 :class="$style.title">
+            UNE HISTOIRE <br> D’humains
+          </h3>
+          <div :class="$style.desc">
+            <p>
               1 heure, c’est le temps moyen que vous passez en cuisine
               quotidiennement. Ne réfléchissez plus à vos repas et gagnez du temps
               sur vos courses, votre cuisine et la vaisselle grâce à Persil &
               Romarin.
             </p>
-            <Button>
+            <Button :class="$style.button">
               En savoir plus
             </Button>
           </div>
@@ -30,124 +31,172 @@
 import {defineComponent} from 'vue'
 import Button  from '@/components/Button/Button.vue';
 import Persil from "../../assets/persilromain.png"
+
 export default defineComponent({
   name: 'UneHistoire',
 
   components: {
     Button,
   },
-  setup(){
-    return{Persil}
-  }})
+
+  setup() {
+    return{
+      Persil
+    }
+  }
+})
 </script>
 
 <style lang="scss" module>
-.histoireback {
-  height: 100vh;
+.container {
 
-  background-color: #f4eee7;
-}
+  background-color: $cream;
 
-.texthistoire {
-  max-width: 50%;
-}
+  @media only screen and (min-width: 720px) {
+    display: flex;
+    align-items: center;
+    padding: 0 24px;
+  }
 
-.histoirecentre {
-  height: 503px;
-  margin: auto;
-  padding: 10%;
-
-  text-align: center;
-}
-
-.histoiregauche {
-  float: left;
-  height: 503px;
-
-  & > img {
-    width: 420px;
-
-    background-color: #20332b;
+  @media only screen and (min-width: 1200px) {
+    height: 100vh;
+    min-height: 720px;
+    max-height: 1000px;
   }
 }
 
-.histoiredroite {
-  position: relative;
-  bottom: 0;
+.inner {
+  @media only screen and (min-width: 720px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
 
-  float: right;
-  width: 50%;
-  height: 503px;
+.view {
+  display: block;
+  width: 100%;
+  max-width: 375px;
+  height: auto;
+  margin: 0 auto;
 
+  @media only screen and (min-width: 720px) {
+    width: 50%;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    max-width: 650px;
+  }
+}
+
+.content {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 132px 24px;
+
+  @media only screen and (min-width: 1200px) {
+    align-self: flex-end;
+    height: 100%;
+    padding-bottom: 0;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    max-width: 804px;
+  }
+}
+
+.desc {
   padding-bottom: 30px;
-  border-bottom: 2px solid #20332b;
 
-  & > div {
-    position: absolute;
-    bottom: 40px;
-  }
-
-  & > div > p {
-    margin-top: 30px;
-
-    text-align: left;
-  }
-
-  & > div > button {
-    float: left;
-    margin-top: 30px;
+  @media only screen and (min-width: 1200px) {
+    max-width: 350px;
+    margin-left: 80px;
+    border-bottom: 1px $black solid;
   }
 }
 
-.h3left {
+.title {
   @include typo-big-title;
 
-  color: #175b06;
+  color: $cactus;
   font-size: 46px;
   text-align: left;
-  text-transform: uppercase;
 }
 
-@media only screen and (max-width: 720px) {
-  .histoiredroite {
-    position: inherit;
-    bottom: 0;
+.desc > p {
+  margin-top: 32px;
+  margin-right: 32px;
+  margin-left: 64px;
+  padding-bottom: 24px;
+  border-bottom: 1px $black solid;
 
-    float: inherit;
-
-    width: 100%;
-    height: 440px;
-    padding-right: 20px;
-    padding-bottom: 30px;
-    padding-left: 20px;
-    border-bottom: 2px solid #20332b;
-
-    & > div {
-      position: inherit;
-      bottom: 40px;
-
-      width: 100%;
-    }
-  }
-
-  .histoiregauche {
-    float: none;
-
-    & > img {
-      float: none;
-      width: 100vw;
-    }
-  }
-
-  .histoirecentre {
-    height: fit-content;
-    padding: 0%;
-  }
-
-  .histoireback {
-    height: auto;
-
-    background-color: #f4eee7;
+  @media only screen and (min-width: 1200px) {
+    margin: 0;
+    margin-top: 64px;
+    border-bottom: 0;
   }
 }
+
+.button {
+  display: block;
+  width: 100%;
+  max-width: 270px;
+  margin: 0 auto;
+  margin-top: 42px;
+
+  font-weight: 400;
+
+  font-size: 12px !important;
+
+  @media only screen and (min-width: 1200px) {
+    max-width: none;
+    margin: 0;
+  }
+}
+
+// @media only screen and (max-width: 720px) {
+//   .content {
+//     position: inherit;
+//     bottom: 0;
+
+//     float: inherit;
+
+//     width: 100%;
+//     height: 440px;
+//     padding-right: 20px;
+//     padding-bottom: 30px;
+//     padding-left: 20px;
+//     border-bottom: 2px solid #20332b;
+
+//     & > div {
+//       position: inherit;
+//       bottom: 40px;
+
+//       width: 100%;
+//     }
+//   }
+
+//   .view {
+//     float: none;
+
+//     & > img {
+//       float: none;
+//       width: 100vw;
+//     }
+//   }
+
+//   .inner {
+//     height: fit-content;
+//     padding: 0%;
+//   }
+
+//   .container {
+//     height: auto;
+
+//     background-color: #f4eee7;
+//   }
+// }
 </style>
