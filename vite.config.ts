1177407562f19fa58e5963a28b8cdd5c6738a051
Clement Po <clement.pommerie@gmail.com>
2021-06-22 13:30:46 +0200

    Last one working

--- vite.config.ts
+++ vite.config.ts
@@ -7,44 +7,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      manifest: {
-        "theme_color": "#f69435",
-        "background_color": "#f69435",
-        "display": "standalone",
-        "scope": "/",
-        "start_url": "/",
-        "name": "Coucou",
-        "short_name": "kou",
-        "description": "Oui",
-        "icons": [
-            {
-                "src": "/icon-192x192.png",
-                "sizes": "192x192",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-256x256.png",
-                "sizes": "256x256",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-384x384.png",
-                "sizes": "384x384",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-512x512.png",
-                "sizes": "512x512",
-                "type": "image/png"
-            }
-        ]
-    },
-      workbox: {
-        // workbox options for generateSW
-      }
-    })
-    
   ],
 
   resolve: {
@@ -66,3 +28,4 @@ export default defineConfig({
   }
 
 })
+
