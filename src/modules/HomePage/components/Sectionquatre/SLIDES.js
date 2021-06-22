e9ae6e3d7a94ecd3069c523c5f6a303b21e2b9bb
Félix Bouveret <bouveret.felix@gmail.com>
2021-06-22 11:57:02 +0200

    feat(sectionquatre): add carousel

---
+++ src/modules/HomePage/components/Sectionquatre/SLIDES.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    name: 'FABRICE J.',
+    text: '“UN service de qualité, le plaisir d’échanger avec le cuisinier, les plats préts pour la semaine. Large choix de plats et quatintées satisfaisantes.”',
+  },
+  {
+    name: 'Clement P.',
+    text: '“UN service de qualité préts pour la semaine. Large choix de plats et quatintées satisfaisantes.”',
+  },
+  {
+    name: 'Jeanne M.',
+    text: '“UN service de qualité, le plaisir d’échanger avec le cuisinier, les plats préts pour la semaine. Large choix de plats et quatintées satisfaisantes.”',
+  },
+  {
+    name: 'Camille T.',
+    text: '“UN service de qualité, le plaisir d’échanger avec le cuisinier, l quatintées satisfaisantes.”',
+  },
+  {
+    name: 'Siwoo K.',
+    text: '“UN service de qualité, le plaisir d’échanger avec le cuisinier, les plats préts pour la semaine. ”',
+  },
+
+]
--- src/modules/HomePage/components/Sectionquatre/Sectionquatre.vue
+++ src/modules/HomePage/components/Sectionquatre/Sectionquatre.vue
@@ -1,44 +1,125 @@
 <template>
   <section :class="$style.sectiongreen">
     <Wrapper :class="$style.wrappercustomsection">
-      <h4 :class="$style.sectiongreenh4">
-        “UN service de qualité, le plaisir d’échanger avec le cuisinier, les plats préts pour la semaine. Large choix de plats et quatintées satisfaisantes.”
-      </h4>
+      <div
+        v-for="({text, name}, slideIndex) in slides"
+        v-show="activeSlideId === slideIndex"
+        :key="slideIndex.toString()"
+        :class="$style.item"
+      >
+        <p :class="$style.comment">
+          {{ text }}
+        </p>
+        <div :class="$style.divider" />
+        <p :class="$style.author">
+          {{ name }}
+        </p>
+      </div>
+      <div :class="$style.buttonContainer">
+        <button
+          :class="[$style.button, {[$style.isActive]: hasPrevious}]"
+          @click="decrement"
+        >
+          <img
+            :src="ArrowLeft"
+            alt=""
+          >
+        </button>
+        <button
+          :class="[$style.button, {[$style.isActive]: hasNext}]"
+          @click="increment"
+        >
+          <img
+            :src="ArrowRight"
+            alt=""
+          >
+        </button>
+      </div>
     </Wrapper>
   </section>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {computed, defineComponent, ref} from 'vue'
+import SLIDES from './SLIDES.js'
+import ArrowLeft from '@/assets/icons/circular-arrow-left.svg'
+import ArrowRight from '@/assets/icons/circular-arrow-right.svg'
 
 export default defineComponent({
   name: 'Sectionquatre',
+  setup() {
+    const slides = ref(SLIDES)
+    const activeSlideId = ref(0)
 
+    const hasPrevious = computed(() => !!activeSlideId.value)
+    const hasNext = computed(() => activeSlideId.value < slides.value.length -1)
+
+    const increment = () => {
+      activeSlideId.value++
+    }
+    const decrement = () => {
+      activeSlideId.value--
+    }
+
+    return {
+      activeSlideId,
+      slides,
+      ArrowLeft,
+      ArrowRight,
+      hasPrevious,
+      hasNext,
+      increment,
+      decrement
+    } 
+  }
 })
 </script>
 
 <style lang="scss" module>
 .sectiongreen {
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
+  padding: 0 24px;
   padding-top: 120px;
 
-  background-color: #20332b;
+  background-color: $green;
 }
 
 .wrappercustomsection {
   text-align: center;
 }
 
-.sectiongreenh4 {
-  @include typo-title;
-  width: 710px;
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 350px;
+}
+
+.comment {
+  @include typo-headline;
+  max-width: 510px;
   margin: auto;
 
-  color: #fbfbfb;
+  color: $white;
 
   line-height: 31px;
   text-align: center;
+}
+
+.divider {
+  flex-grow: 1;
+  width: 1px;
+  height: 100%;
+  margin: 32px;
+
+  background-color: $white;
+}
+
+.author {
+  @include typo-body;
+  color: $white;
 }
 
 @media only screen and (max-width: 720px) {
@@ -46,11 +127,35 @@ export default defineComponent({
     display: flex;
     height: 100vh;
   }
+}
 
-  .sectiongreenh4 {
-    width: 90%;
+.buttonContainer {
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+  margin-top: 8px;
+}
 
-    font-size: 20px;
+.button {
+  padding: 0;
+  border: 0;
+  border-radius: 64px;
+
+  background-color: transparent;
+  cursor: pointer;
+
+  opacity: 0.2;
+
+  pointer-events: none;
+
+  &.isActive {
+    opacity: 1;
+
+    pointer-events: all;
+  }
+
+  img {
+    display: block;
   }
 }
 </style>
